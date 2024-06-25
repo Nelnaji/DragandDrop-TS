079555d2ff9d5ee731c1fb4b556580aa790acfd6
naofel <nawfal.elkhaznagi@gmail.com>
2024-06-25 21:23:32 +0200

    added classes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,42 @@
-// Code goes here!
+class ProjectInput{
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
+    constructor() {
+        // all elements : 
+        this.templateElement = document.querySelector('#project-input')! as HTMLTemplateElement; 
+        this.hostElement = document.querySelector('#app')! as  HTMLDivElement;
+        
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element= importedNode.firstElementChild as HTMLFormElement;
+        this.element.id='user-input';
+
+
+        this.titleInputElement = this.element.querySelector('#title')! as  HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description')! as  HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people')! as  HTMLInputElement;
+    
+
+        this.configurator();
+        this.nodeInserter();
+    }
+
+    private submitHandler(event:Event){
+        event.preventDefault();
+        console.log(this.titleInputElement.value)
+    };
+    private configurator(){
+        this.element.addEventListener('submit', this.submitHandler.bind(this))
+    };
+
+    private nodeInserter() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+
+}
+
+const prjInput = new ProjectInput();
