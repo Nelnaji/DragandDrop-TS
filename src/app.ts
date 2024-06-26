bee2ff09755b78c8a1965ac1cf588ee6dbf4cdc2
naofel <nawfal.elkhaznagi@gmail.com>
2024-06-26 18:06:32 +0200

    stopping for now to focus on how to us ts with react and node

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+// autobind decorator
+function autobind(_:any, _2:string, descriptor: PropertyDescriptor){
+
+
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get(){}
+    }
+}
+
+// project input class
 class ProjectInput{
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -15,7 +27,7 @@ class ProjectInput{
         this.element= importedNode.firstElementChild as HTMLFormElement;
         this.element.id='user-input';
 
-
+// this.element because and not document, because the element is still under construction ²
         this.titleInputElement = this.element.querySelector('#title')! as  HTMLInputElement;
         this.descriptionInputElement = this.element.querySelector('#description')! as  HTMLInputElement;
         this.peopleInputElement = this.element.querySelector('#people')! as  HTMLInputElement;
@@ -25,9 +37,26 @@ class ProjectInput{
         this.nodeInserter();
     }
 
+//this is how you setup tuple [ele,ele,ele]
+    private gatherUserInput():[string,string,number] {
+        const enteredTitile = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        if(
+            enteredTitile.trim().length === 0 || 
+            enteredDescription.trim().length === 0 || 
+            enteredPeople.trim().length === 0
+            ) {
+
+                
+        }
+    }
+    @autobind
     private submitHandler(event:Event){
         event.preventDefault();
         console.log(this.titleInputElement.value)
+        const userInput = this.gatherUserInput();
     };
     private configurator(){
         this.element.addEventListener('submit', this.submitHandler.bind(this))
